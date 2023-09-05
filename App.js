0f354d6b5aaeddeb01fdab24f11f0dd4daa81e1f
Heshiel Jay Juntilla <hershieljayjuntilla@gmail.com>
2023-09-05 22:06:26 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { styles } from './style/Styles.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+
+        <View style={[
+          styles.mainheader,
+          {backgroundColor: '#96C291'}
+        ]}>
+
+        <View style={styles.contentmargin}>
+        <Image 
+        source={require('./src/image/profile.png')} 
+        style={{width: 100, height: 100, borderRadius: 50}}
+        />
+          <Text style={[styles.name,
+            {fontSize: 30, fontWeight: "bold"}
+          ]}>Hershiel Jay Juntilla</Text>
+          <Text style={[styles.name,
+            {fontSize: 20}
+          ]}>BS Info Tech 3</Text>
+        </View>
+
+      </View>
+
+        <View style={styles.container2}>
+          <Text style={[
+          {color: 'black' ,fontSize: 25, marginRight: 230}
+          ]}>My Subjects</Text>
+
+          <View style={styles.subheader}>
+
+          <Text style={styles.subtext}>ELEC 2</Text>
+          <Text style={styles.subtxt2}>Web Development/ Web Enterprise{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>ELEC 3</Text>
+          <Text style={[
+              {marginRight: 123, marginTop: 23}   
+            ]}>Mobile Application{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 311</Text>
+          <Text style={[
+            {marginRight: 105, marginTop: 23}   
+          ]}>Software Engineering{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 312</Text>
+          <Text style={[
+            {marginRight: 27, marginTop: 23, fontSize: 13}   
+          ]}>Information Assurance and Security 2{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 313</Text>
+          <Text style={[
+            {marginRight: 107, marginTop: 23}   
+          ]}>Quantitative Methods{'\n'}Units: 3</Text>
+          </View>
+          <Text style={[
+            {color: '#525FE1' ,
+            fontSize: 15,
+            marginTop: 15,
+            marginLeft: 205}
+            ]}>Click here to see more...</Text>
+      </View>
     </View>
+
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style/Styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'space-evenly'
+      
+    },
+    container2: {
+        flex: 0,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        marginTop: 20,
+        alignItems: 'center'
+        
+    },
+    mainheader: {
+        width: 370,
+        height: 230,
+        marginTop: 40,
+        borderTopRightRadius: 50,
+        borderBottomRightRadius: 50
+    },
+    subheader: {
+        width: 370,
+        height: 80,
+        marginTop: 10,
+        backgroundColor: '#F0EEED',
+        borderBottomRightRadius: 15,
+        borderTopLeftRadius: 15,
+        borderBottomLeftRadius: 15,
+        borderTopRightRadius: 15,
+        flexDirection: 'row',
+        alignItems: 'center'
+
+    },
+    name: {
+        color: 'white',
+    },
+    contentmargin: {
+        marginLeft: 15,
+        marginTop: 30
+    },
+    subtext: {
+        color: 'black' ,
+        fontSize: 20,
+        fontWeight: 'bold',
+        flex: 1,
+        marginLeft: 25,
+        marginTop: 25,
+    
+    },
+    subtxt2: {
+        fontSize: 13,
+        marginRight: 40,
+        marginTop: 23 
+    }
+  });
